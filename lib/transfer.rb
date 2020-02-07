fdd811abc61003e4cc906512363e9423e2023505
Kevin Villalobos <kevinv15v@gmail.com>
2020-02-07 22:52:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,6 +15,7 @@ class Transfer
   end
 
   def execute_transaction
+    binding.pry
     if @sender.balance < @amount
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
