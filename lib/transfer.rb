09c1f9ad5f6da39a4c22a751771541d4a04931de
Kevin Villalobos <kevinv15v@gmail.com>
2020-02-07 22:59:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,6 +26,9 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
   end
 
   def reverse_transfer
